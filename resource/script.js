68e099d0a2a37950fb3b15483fe28668225aff62
Abdul Rehan <rehan.dxtr.sq@gmail.com>
2019-12-05 16:54:39 +0530

    java script pretty

--- resource/script.js
+++ resource/script.js
@@ -1,33 +1,34 @@
 var defa = ["meter/sec", "Kelvin"];
 var metric = ["meter/sec", "Celcius"];
 var imperial = ["miles/hour", "Fahrenheit"];
-function getData(){
-    
-    var city = document.getElementById('city').value;
+function getData() {
+    var city = document.getElementById("city").value;
     var radioValue = document.getElementById("unit").value;
- var url="https://api.openweathermap.org/data/2.5/weather?q=" + city +"&units=" +radioValue +"&APPID=784970cd25d49bd7c2e4f9c019631272";
-    fetch(url).then(result=>result.json()).then((data)=>{
-       try{
-           if (radioValue == "default") {
-                        var widget = show(data, defa);
-                    } else if (radioValue == "metric") {
-                        var widget = show(data, metric);
-                    } else if (radioValue == "imperial") {
-                        var widget = show(data, imperial);
-                    }
-        document.getElementById("result").innerHTML=widget;
-        document.getElementById("city").innerHTML="";
-       }
-        catch{
-    alert("NOt Found");
-        document.getElementById("city").innerHTML="";
-    }
-    })
-    
-    
-    
+    var url =
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=" +
+        radioValue +
+        "&APPID=784970cd25d49bd7c2e4f9c019631272";
+    fetch(url)
+        .then(result => result.json())
+        .then(data => {
+            try {
+                if (radioValue == "default") {
+                    var widget = show(data, defa);
+                } else if (radioValue == "metric") {
+                    var widget = show(data, metric);
+                } else if (radioValue == "imperial") {
+                    var widget = show(data, imperial);
+                }
+                document.getElementById("result").innerHTML = widget;
+                document.getElementById("city").innerHTML = "";
+            } catch {
+                alert("NOt Found");
+                document.getElementById("city").innerHTML = "";
+            }
+        });
 }
-
 
 function show(data, unit) {
     return (
